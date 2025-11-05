71c36d2c840cb1260057d6f787d07ebf06cf841e
theduckinduck <alecjmartinez12@gmail.com>
2025-11-05 20:32:10 +0000

    commit and push

--- script.js
+++ script.js
@@ -27,6 +27,7 @@ if (idNum % 2 == 1) {
 let checkbox = document.createElement("input");
 checkbox.type = "checkbox";
 checkbox.id = "checkbox" + idNum;
+checkbox.addEventListener("change", removeTask);
 
 // create label
 let label = document.createElement("label");
@@ -43,4 +44,31 @@ taskDiv.appendChild(label);
 
 //add the dask div to the list
 taskList.appendChild(taskDiv);
+}
+
+function removeTask(event) {
+    // get the id of the div to remove
+    let checkboxId = event.target.id;
+    let idNum = checkboxId.substring(8);
+    let taskDiv = document.getElementById("task" + idNum);
+    taskDiv.classList.add("remove");
+
+    // get the task list container
+    let taskList = document.getElementById("task-list");
+
+    // remove task div from task list
+    setTimeout(function() {
+         taskList.removeChild(taskDiv);
+         fixTaskColors();
+    }, 600)
+}
+
+function fixTaskColors() {
+    let taskList = document.getElementById("task-list");
+    for (let i = 0; i < taskList.childElementCount; i++) {
+        taskList.children[i].style.backgroundColor = "dodgerblue";
+        if (i % 2 == 1) {
+            taskList.children[i].style.backgroundColor = "darkturquoise";
+        }
+    }
 }
